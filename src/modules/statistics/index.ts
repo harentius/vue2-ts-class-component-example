1c921941da183235918d69f93c152d5beb585029
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2022-02-22 21:25:58 +0200

    fix typo

--- src/modules/statistics/index.ts
+++ src/modules/statistics/index.ts
@@ -2,6 +2,6 @@ import { eventBus } from '@/eventBus'
 import { Product } from '../client'
 
 eventBus.$on('PRODUCT_CLICK', (product: Product) => {
-  console.warn(`Sending statistics about interes to product with id '${product.id}'`)
+  console.warn(`Sending statistics about interest to product with id '${product.id}'`)
   window.open(product.image, '_blank')
 })
